3cf3704ea5f17c0e93131ec3b9754b5b71128628
Michelle Totherow <tym4me13@gmail.com>
2024-01-01 10:47:41 -0600

    migration corrects

--- jennabapi/models/cost.py
+++ jennabapi/models/cost.py
@@ -5,5 +5,5 @@ class Cost(models.Model):
   
     
     label = models.CharField(max_length=50)
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     occurrence = models.ForeignKey(Occurrence, on_delete=models.CASCADE, related_name="costs")
