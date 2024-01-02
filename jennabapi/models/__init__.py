8074384a9d766f19d4be8e26d182d836bdf397bd
Michelle Totherow <tym4me13@gmail.com>
2024-01-02 08:51:11 -0600

    refactor to include username

--- jennabapi/models/__init__.py
+++ jennabapi/models/__init__.py
@@ -2,7 +2,6 @@ from .shuttleUser import ShuttleUser
 from .job import Job
 from .vehicle import Vehicle
 from .cost import Cost
-from .vehicleCost import VehicleCost
 from .client import Client
 from .occurrence import Occurrence
 
